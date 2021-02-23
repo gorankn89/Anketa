1fe5bfd783db9406bc687aba6a60e57ce3a5ca7f
gorankn89 <61165552+gorankn89@users.noreply.github.com>
2021-02-23 14:34:37 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -1,1 +1,35 @@
-console.log(`test`);
+let popunjeniElementi = true;
+
+function zastita() {
+    popunjeniElementi = true;
+    for (let i = 10; i < 20; i++) {
+        let izbor = `izbor${i}`;
+        console.log(izbor);
+        let vrednost = document.getElementById(izbor).value;
+        if (vrednost !== "" && popunjeniElementi === true) {
+
+        } else {
+            if (popunjeniElementi === true) {
+                popunjeniElementi = false;
+                alert(`Molimo proverite da li su polja popunjena!!!`);
+            }
+        }
+    }
+    if (popunjeniElementi === true) {
+        writeData();
+    }
+}
+
+function clearField() {
+    for (let i = 10; i < 65; i++) {
+        try {
+
+            document.getElementById(`izbor${i}`).value = "";
+
+
+        } catch (error) {
+
+        }
+
+    }
+}
